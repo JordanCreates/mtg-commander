198b21450c9db2e6ca095d3d8341a2f32ca99621
Jordan <jordanyu@hotmail.co.uk>
2020-01-07 19:34:41 +0000

    Updates

---
+++ precache-manifest.38204d0cd3919967d22c034aa620c49d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ff2c60c32ae0cb01d964591656f579",
+    "url": "/mtg-commander/index.html"
+  },
+  {
+    "revision": "d6102ccdaebdf2a7925e",
+    "url": "/mtg-commander/static/css/main.c7ef5088.chunk.css"
+  },
+  {
+    "revision": "bbdc121e0ef76b25a766",
+    "url": "/mtg-commander/static/js/2.e2445a86.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/mtg-commander/static/js/2.e2445a86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6102ccdaebdf2a7925e",
+    "url": "/mtg-commander/static/js/main.8a6c6d81.chunk.js"
+  },
+  {
+    "revision": "17a8ff27d28fb0889d85",
+    "url": "/mtg-commander/static/js/runtime-main.e7fe347a.js"
+  }
+]);
